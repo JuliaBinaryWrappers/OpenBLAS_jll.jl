5c05ebaefbb432d5ac6d61ec4f3c80e49eaebb58
jlbuild <juliabuildbot@gmail.com>
2019-12-31 20:11:43 +0000

    OpenBLAS_jll build 0.3.7+2

--- src/wrappers/i686-w64-mingw32-libgfortran4.jl
+++ src/wrappers/i686-w64-mingw32-libgfortran4.jl
@@ -24,11 +24,13 @@ const libopenblas = "libopenblas.dll"
 Open all libraries
 """
 function __init__()
-    global prefix = abspath(joinpath(@__DIR__, ".."))
+    global artifact_dir = abspath(artifact"OpenBLAS")
 
     # Initialize PATH and LIBPATH environment variable listings
     global PATH_list, LIBPATH_list
-    global libopenblas_path = abspath(joinpath(artifact"OpenBLAS", libopenblas_splitpath...))
+    # We first need to add to LIBPATH_list the libraries provided by Julia
+    append!(LIBPATH_list, [Sys.BINDIR, joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
+    global libopenblas_path = normpath(joinpath(artifact_dir, libopenblas_splitpath...))
 
     # Manually `dlopen()` this right now so that future invocations
     # of `ccall` with its `SONAME` will find this path immediately.
