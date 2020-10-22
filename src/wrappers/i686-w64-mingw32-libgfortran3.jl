df6883474fd562cd93308822557c42271a695d98
jlbuild <juliabuildbot@gmail.com>
2020-10-22 01:29:58 +0000

    OpenBLAS_jll build 0.3.10+1

--- src/wrappers/i686-w64-mingw32-libgfortran3.jl
+++ src/wrappers/i686-w64-mingw32-libgfortran3.jl
@@ -2,52 +2,15 @@
 export libopenblas
 
 using CompilerSupportLibraries_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "PATH"
-LIBPATH_default = ""
-
-# Relative path to `libopenblas`
-const libopenblas_splitpath = ["bin", "libopenblas.dll"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libopenblas_path = ""
-
-# libopenblas-specific global declaration
-# This will be filled out by __init__()
-libopenblas_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libopenblas = "libopenblas.dll"
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("OpenBLAS")
+JLLWrappers.@declare_library_product(libopenblas, "libopenblas.dll")
 function __init__()
-    global artifact_dir = abspath(artifact"OpenBLAS")
+    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll)
+    JLLWrappers.@init_library_product(
+        libopenblas,
+        "bin\\libopenblas.dll",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (CompilerSupportLibraries_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (CompilerSupportLibraries_jll.LIBPATH_list,))
-
-    global libopenblas_path = normpath(joinpath(artifact_dir, libopenblas_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libopenblas_handle = dlopen(libopenblas_path)
-    push!(LIBPATH_list, dirname(libopenblas_path))
-
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ';')
-    global LIBPATH = join(vcat(LIBPATH_list, [Sys.BINDIR, joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ';')
-
-    
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
