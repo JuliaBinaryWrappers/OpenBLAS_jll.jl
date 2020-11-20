667d876b9ad63dab3e70e45a904a14ee47438004
jlbuild <juliabuildbot@gmail.com>
2020-11-20 03:50:55 +0000

    OpenBLAS_jll build 0.3.12+0

--- src/wrappers/x86_64-apple-darwin-libgfortran5.jl
+++ src/wrappers/x86_64-apple-darwin-libgfortran5.jl
@@ -8,7 +8,7 @@ function __init__()
     JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll)
     JLLWrappers.@init_library_product(
         libopenblas,
-        "lib/libopenblas64_.0.3.10.dylib",
+        "lib/libopenblas64_.0.3.12.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
