907b47a950f213accb126eae702f02255cd2225f
jlbuild <juliabuildbot@gmail.com>
2021-02-18 00:36:41 +0000

    OpenBLAS_jll build 0.3.10+4

--- src/wrappers/aarch64-apple-darwin-libgfortran5.jl
+++ src/wrappers/aarch64-apple-darwin-libgfortran5.jl
@@ -3,12 +3,12 @@ export libopenblas
 
 using CompilerSupportLibraries_jll
 JLLWrappers.@generate_wrapper_header("OpenBLAS")
-JLLWrappers.@declare_library_product(libopenblas, "@rpath/libopenblas64_.dylib")
+JLLWrappers.@declare_library_product(libopenblas, "@rpath/libopenblas.dylib")
 function __init__()
     JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll)
     JLLWrappers.@init_library_product(
         libopenblas,
-        "lib/libopenblas64_.0.3.10.dylib",
+        "lib/libopenblas.0.3.10.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
